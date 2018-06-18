78c6a591b7c4cfecd7ed2390571bc725601abf23
vinkky <nezinaugoblinas5@gmail.com>
2018-06-18 10:00:11 +0300

    added profile and user schema, jwt, validations

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://Tautvydas:lopas123@ds231549.mlab.com:31549/devconnector'
+    mongoURI: 'mongodb://Tautvydas:lopas123@ds231549.mlab.com:31549/devconnector',
+    secretOrKey: 'secret'
 };
